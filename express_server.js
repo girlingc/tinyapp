2d81d09534d50bb709a7f4494a83a161489213f8
Christian Girling <girling.christian@gmail.com>
2022-01-24 23:12:57 -0800

    Added body parser to return input from new URLs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs");
 const urlDatabase = {
@@ -21,6 +23,7 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
@@ -35,6 +38,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
