d032250a8c5946f9cb294bee70d1c5d90b17d1cd
Christian Girling <girling.christian@gmail.com>
2022-01-27 03:50:34 -0800

    Refactor: similar GET and POST requests are closer

--- express_server.js
+++ express_server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Generates random string for short URLs
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-const generateRandomString =(length) => {
+const generateRandomString = (length) => {
   let result = '';
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
@@ -24,15 +24,15 @@ const generateRandomString =(length) => {
 app.set("view engine", "ejs");
 
 const users = {  "userRandomID": {
-  id: "userRandomID", 
-  email: "user@example.com", 
+  id: "userRandomID",
+  email: "user@example.com",
   password: "purple-monkey-dinosaur"
 },
 "user2RandomID": {
-  id: "user2RandomID", 
-  email: "user2@example.com", 
+  id: "user2RandomID",
+  email: "user2@example.com",
   password: "dishwasher-funk"
-}}
+}};
 // Temporary Database until we create an actual one
 const urlDatabase = {
   b6UTxQ: {
@@ -54,23 +54,34 @@ const findEmail = (email, database) => {
   return undefined;
 };
 
+// Function to build accumulate an owners URLs
 const userURLs = (id) => {
   let urls = {};
-
   for (const url in urlDatabase) {
     if (urlDatabase[url].userID === id) {
-      urls[url] = urlDatabase[url]
+      urls[url] = urlDatabase[url];
     }
   }
-  return urls
-}
+  return urls;
+};
 
-// Routing configuration // 
+// Routing configuration
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-//
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+////////
+////////
+// Displays user's URL's
 app.get("/urls", (req, res) => {
   const myURLs = userURLs(req.cookies["user_id"]);
   let templateVars = {
@@ -80,7 +91,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+    userID: req.cookies["user_id"]
+  };
+  res.redirect(`/urls/${shortURL}`);
+});
+
+////////
+////////
+// Create new URL
 app.get("/urls/new", (req, res) => {
   if (req.cookies["user_id"]) {
     let templateVars = { user: users[req.cookies["user_id"]] };
@@ -90,7 +112,9 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
-//
+////////
+////////
+// Update or delete an existing URL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
@@ -100,43 +124,17 @@ app.get("/urls/:shortURL", (req, res) => {
   if (req.cookies["user_id"] === urlDatabase[templateVars.shortURL].userID) {
     res.render("urls_show", templateVars);
   } else {
-    res.statusCode = 404
-    res.send("<h2>404<br>You need to log in to access this URL</h2>")
-  }
-});
-
-//
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-//
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-// Need to find bug of why if statement is not working
-app.get("/u/:shortURL", (req, res) => {
-  if (urlDatabase[req.params.shortURL].longURL) {
-    res.redirect(urlDatabase[req.params.shortURL].longURL);
-  } else {
     res.statusCode = 404;
-    res.send('<h2>404 Not Found<br>This short URL does not exist in the database.</h2>')
+    res.send("<h2>404<br>You need to log in to access this URL</h2>");
   }
 });
 
-app.get("/login", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
-  res.render("urls_login", templateVars)
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL].longURL = req.body.updatedURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
-//
-app.get("/register", (req, res) => {
-  let templateVars = { user: users[req.cookies["user_id"]] };
-  res.render("urls_registration", templateVars);
-});
-
-// Deletes URL from database and redirects to /urls //
 app.post("/urls/:shortURL/delete", (req, res) => {
   if (req.cookies["user_id"] === urlDatabase[req.params.shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
@@ -144,24 +142,29 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-// Adds random string to create short URL //
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString(6);
-  urlDatabase[shortURL] = {
-    longURL: req.body.longURL,
-    userID: req.cookies["user_id"]
+
+
+////////
+////////
+// Redirects user to existing long URL
+// Need to find bug of why if statement is not working
+app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL].longURL) {
+    res.redirect(urlDatabase[req.params.shortURL].longURL);
+  } else {
+    res.statusCode = 404;
+    res.send('<h2>404 Not Found<br>This short URL does not exist in the database.</h2>');
   }
-  res.redirect(`/urls/${shortURL}`);
 });
 
-// Update URL //
-app.post("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  urlDatabase[shortURL].longURL = req.body.updatedURL;
-  res.redirect(`/urls/${shortURL}`);
+////////
+////////
+// Logs in a user, or logs them out
+app.get("/login", (req, res) => {
+  let templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("urls_login", templateVars);
 });
 
-// Set up login / logout //
 app.post("/login", (req, res) => {
   const user = findEmail(req.body.email, users);
   if (user) {
@@ -170,19 +173,26 @@ app.post("/login", (req, res) => {
       res.redirect("/urls");
     } else {
       res.statusCode = 403;
-      res.send("<h2>403<br>You entered the wrong password</h2>")
+      res.send("<h2>403<br>You entered the wrong password</h2>");
     }
   } else {
     res.statusCode = 403;
-    res.send("<h2>403<br>This account is not registered</h2>")
+    res.send("<h2>403<br>This account is not registered</h2>");
   }
 });
 
-//
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id", req.body.userID);
-  console.log("DATABASE", urlDatabase)
-  res.redirect("/urls")
+  console.log("DATABASE", urlDatabase);
+  res.redirect("/urls");
+});
+
+////////
+////////
+// Registers a new user
+app.get("/register", (req, res) => {
+  let templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("urls_registration", templateVars);
 });
 
 app.post("/register", (req, res) => {
@@ -193,23 +203,20 @@ app.post("/register", (req, res) => {
         userID,
         email: req.body.email,
         password: req.body.password
-      }
+      };
       res.cookie("user_id", userID);
       res.redirect("/urls");
     } else {
       res.statusCode = 400;
-      res.send("<h2>400 Bad Request<br>That Email has already been registered</h2>")
+      res.send("<h2>400 Bad Request<br>That Email has already been registered</h2>");
     }
   } else {
     res.statusCode = 400;
-    res.send("<h2>400 Bad Request<br>Please enter a valid Email and Password</h2>")
+    res.send("<h2>400 Bad Request<br>Please enter a valid Email and Password</h2>");
   }
 });
-
-
 
 // Making sure server is up on expected port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
